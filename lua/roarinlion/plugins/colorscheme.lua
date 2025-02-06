2c56385efeeaea35858c383217fe8c22168ef27b
roarinLion <roarinlion@pm.me>
2025-02-06 09:33:20 -0800

    save

--- lua/roarinlion/plugins/colorscheme.lua
+++ lua/roarinlion/plugins/colorscheme.lua
@@ -6,7 +6,7 @@ return {
 
     local bg = "#011628"
     local bg_dark = "#011423"
-    local bg_highlight = "#143652"
+    local bg_highlight = "#011628"
     local bg_search = "#0A64AC"
     local bg_visual = "#275378"
     local fg = "#CBE0F0"
